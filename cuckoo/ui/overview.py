ed7840fe5f7ea258d4cd68e3bfc43679461c1b9e
John Vrbanac <john.vrbanac@linux.com>
2015-04-28 21:46:56 -0500

    Pulling out the alarm row into a custom widget
    Provides the ability to easily identify the alarm associated
    with each widget. Also, hooking up the alarm switch.

--- cuckoo/ui/overview.py
+++ cuckoo/ui/overview.py
@@ -5,23 +5,34 @@ import random
 from cuckoo import utils, alarm
 from gi.repository import Gtk
 
+class AlarmRow(Gtk.Box):
+    def switch_toggled(self, switch, state):
+        if state:
+            self.alarm.activate()
+        else:
+            self.alarm.deactivate()
 
-class OverviewWindow(Gtk.Window):
+    def __init__(self, alarm):
+        super().__init__(orientation=Gtk.Orientation.HORIZONTAL)
+        self.alarm = alarm
 
-    def more_btn_clicked(self, widget):
-        print('bam')
-
-    def create_header_bar(self):
-        bar = Gtk.HeaderBar()
-        bar.set_title('Cuckoo')
-        bar.set_show_close_button(True)
-        bar.set_property('border-width', 0)
-
+        time_box = self.create_time_label(alarm.start_time)
         more_btn = Gtk.Button.new_from_icon_name('view-more-symbolic', 4)
         more_btn.set_relief(Gtk.ReliefStyle.NONE)
-        more_btn.connect('clicked', self.more_btn_clicked)
-        bar.pack_start(more_btn)
-        return bar
+
+        switch = Gtk.Switch()
+        switch.set_valign(Gtk.Align.CENTER)
+        switch.connect('state-set', self.switch_toggled)
+
+        note_label = Gtk.Label()
+        note_label.set_markup(
+            '<span font_size="medium">{}</span>'.format('Good Morning')
+        )
+
+        self.pack_start(time_box, expand=False, fill=True, padding=5)
+        self.pack_start(note_label, expand=True, fill=True, padding=0)
+        self.pack_start(switch, expand=False, fill=True, padding=5)
+        self.pack_start(more_btn, expand=False, fill=True, padding=5)
 
     def create_time_label(self, time_obj):
         time_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
@@ -44,25 +55,23 @@ class OverviewWindow(Gtk.Window):
         time_box.pack_start(ampm_label, expand=False, fill=False, padding=0)
         return time_box
 
-    def create_alarm_row(self, alarm):
-        row = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        time_box = self.create_time_label(alarm.start_time)
+
+class OverviewWindow(Gtk.Window):
+
+    def more_btn_clicked(self, widget):
+        print('bam')
+
+    def create_header_bar(self):
+        bar = Gtk.HeaderBar()
+        bar.set_title('Cuckoo')
+        bar.set_show_close_button(True)
+        bar.set_property('border-width', 0)
+
         more_btn = Gtk.Button.new_from_icon_name('view-more-symbolic', 4)
         more_btn.set_relief(Gtk.ReliefStyle.NONE)
-
-        switch = Gtk.Switch()
-        switch.set_valign(Gtk.Align.CENTER)
-
-        note_label = Gtk.Label()
-        note_label.set_markup(
-            '<span font_size="medium">{}</span>'.format('Good Morning')
-        )
-
-        row.pack_start(time_box, expand=False, fill=True, padding=5)
-        row.pack_start(note_label, expand=True, fill=True, padding=0)
-        row.pack_start(switch, expand=False, fill=True, padding=5)
-        row.pack_start(more_btn, expand=False, fill=True, padding=5)
-        return row
+        more_btn.connect('clicked', self.more_btn_clicked)
+        bar.pack_start(more_btn)
+        return bar
 
     def __init__(self):
         super().__init__(title='Cuckoo')
@@ -83,10 +92,9 @@ class OverviewWindow(Gtk.Window):
                 utils.get_current_time(),
                 utils.get_media_uri('alarm.wav')
             )
-            alarm_obj.activate()
             self.alarm_manager.add(alarm_obj)
             alarms.pack_start(
-                self.create_alarm_row(alarm_obj),
+                AlarmRow(alarm_obj),
                 expand=False,
                 fill=True,
                 padding=5
